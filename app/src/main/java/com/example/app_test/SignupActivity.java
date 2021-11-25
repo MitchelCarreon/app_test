fcaad1553e01a901ddf07a285166ecbe0ddc4bf4
MitchelCarreon <60196056+MitchelCarreon@users.noreply.github.com>
2021-11-24 21:00:04 -0600

    -Added Firebase Database

--- app/src/main/java/com/example/app_test/SignupActivity.java
+++ app/src/main/java/com/example/app_test/SignupActivity.java
@@ -1,6 +1,7 @@
 package com.example.app_test;
 
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -10,13 +11,21 @@ import android.widget.EditText;
 
 import com.example.app_test.Utils.User;
 import com.example.app_test.databinding.ActivitySignupBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 
 public class SignupActivity extends AppCompatActivity {
     private ActivitySignupBinding binding;
-
-
     private EditText email_editText, username_editText;
+
+    private FirebaseAuth mAuth;
+    private FirebaseDatabase db;
+    private DatabaseReference db_reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,13 +33,9 @@ public class SignupActivity extends AppCompatActivity {
         this.binding = ActivitySignupBinding.inflate(getLayoutInflater());
         setContentView(this.binding.getRoot());
 
-        setTitle(getString(R.string.signup_title));
-//        this.binding.inputPasswordretypeArea.setFocusable(true);
-//        this.binding.userAndPwArea.setChecked(true);
+        initComponents();
 
-        this.email_editText = this.binding.inputEmail.getEditText();
-        this.username_editText = this.binding.inputUsername.getEditText();
-
+//       FirebaseDatabase.getInstance().getReference("Users").child("Sample_node").setValue("sample_val");
         this.binding.signupRegisterBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -38,8 +43,21 @@ public class SignupActivity extends AppCompatActivity {
             }
         });
     }
+    public void initComponents() {
+        setTitle(getString(R.string.signup_title));
+        this.email_editText = this.binding.inputEmail.getEditText();
+        this.username_editText = this.binding.inputUsername.getEditText();
 
+        this.db = FirebaseDatabase.getInstance();
+        this.db_reference = this.db.getReference("Users");
+    }
     public void registerUserViaFirebase() {
+        User new_user = new User(
+                this.email_editText.getText().toString(),
+                this.username_editText.getText().toString(),
+                this.binding.signupCountrySpinner.getSelectedCountryName());
 
+        this.db_reference.child(new_user.username).setValue(new_user);
+        // TODO: FIX DB.
     }
 }
