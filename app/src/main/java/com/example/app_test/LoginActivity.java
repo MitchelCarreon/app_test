f3394940f2d072b9a774927873b69e89c0cf24de
MitchelCarreon <60196056+MitchelCarreon@users.noreply.github.com>
2021-12-14 05:21:54 -0600

    Revert "-deleted existing/non-working game txt files. create new adventure."
    This reverts commit bc77c43bb1b86ef3699e28c5b97f3d9eb284d1fa.

--- app/src/main/java/com/example/app_test/LoginActivity.java
+++ app/src/main/java/com/example/app_test/LoginActivity.java
@@ -65,14 +65,13 @@ public class LoginActivity extends AppCompatActivity {
 
                 startActivity(intent);
 
-
             }
         });
     }
 
     private void deleteFile(){ // used for testing
         File dir = getFilesDir();
-        File file = new File(dir,  "test title.txt"); // name of file here. (i.e. adventure title + ".txt")
+        File file = new File(dir,  "t.txt"); // name of file here. (i.e. adventure title + ".txt")
         boolean deleted = file.delete();
     }
 
